40b64162fdef65be9d4cf3825d521bd6bf18680d
MaricarmenBP <maricarmenbuenfil004@gmail.com>
2024-06-12 20:52:05 -0600

    Problem

--- Problems/LinkedList/MergeTwoSortedList.cpp
+++ Problems/LinkedList/MergeTwoSortedList.cpp
@@ -5,11 +5,62 @@ Topic: LinkedList
 
 21. Merge Two Sorted Lists
 
-Approach: 
+---Version 1---
+
+Approach: Simultaneus Iteration Two-Pointers
+The mergeTwoLists_V1 function employs the Simultaneous Iteration Two-Pointer technique. 
+It handles base cases where either list is empty by returning the other list. 
+The function initializes a head pointer to the smaller first node between List1 
+and List2, ensuring proper initialization of the merged list. Using nested loops, 
+it iteratively compares nodes from List2 with nodes from List1, inserting each 
+node from List2 into its correct sorted position within List1. This insertion is 
+managed with a prev pointer to track the node before the insertion point. After 
+merging all nodes of List2 into List1, the function returns head, which points to 
+the beginning of the merged sorted list.
 
 Required Time: 
 Complexity Time: O(n)
 Complexity Space: O(1)
+Assumpitions: 
+
+
+---Version 2---
+
+Approach: Simultaneus Iteration Two-Pointers
+The mergeTwoLists_V2 function employs the Simultaneous Iteration Two-Pointer technique. 
+Initially, it handles the base cases where one of the lists might be empty. The function 
+sets up the head and current pointers of the merged list by comparing the first nodes of 
+both input lists. As it iterates through both lists, it appends the smaller node to the 
+merged list and advances the pointer of the list from which the node was taken. When one 
+list is exhausted, the remaining nodes of the other list are directly appended to the 
+merged list. Ultimately, the function returns the head of the merged list, ensuring that 
+all nodes are in a sorted sequence.
+
+Required Time: 
+Complexity Time: O(n)
+Complexity Space: O(1)
+Assumpitions: 
+
+
+---Version 3---
+
+Approach: Recursion
+The mergeTwoListsRecursion function recursively merges two sorted linked lists into a single 
+sorted linked list. It begins by checking base cases: if either list is empty, it returns the 
+other list. Otherwise, it compares the current nodes of List1 and List2. If the node in List1 
+has a smaller or equal value, it recursively merges the rest of List1 with List2, setting the 
+next pointer of the current node to the result. Conversely, if the node in List2 is smaller, 
+it merges the rest of List2 with List1. This process continues until all nodes from both lists 
+are merged, and the function returns the head of the resulting merged list.
+
+Required Time: 
+
+Complexity Time: 
+O(n)
+
+Complexity Space: 
+If we don’t consider the size of the stack for function calls then O(1) 
+otherwise O(n).
 
 Assumpitions: 
 
@@ -19,176 +70,188 @@ Assumpitions:
 #include <vector>
 using namespace std;
 
-struct Node{
+struct Node {
     int data;
     Node* next;
+
+    Node(int d, Node* n = nullptr) : data(d), next(n) {}
 };
 
-Node* mergeTwoLists_V1( Node* List1, Node* List2 ){
-
-    if (!List1) return List2;
-    if (!List2) return List1;
-
+class LinkedList {
+public:
     Node* head;
 
-    if( List1->data <= List2->data){
-         head = List1;
-    }else{
-         head = List2;
-         List2 = List1;
-         List1 = head; 
+    LinkedList() : head(nullptr) {}
+
+    Node* insertAtFront(Node* head, int data) {
+        Node* newNode = new Node(data);
+        Node* current = head;
+
+        if (head == nullptr) {
+            return newNode;
+        }
+
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        current->next = newNode;
+
+        return head;
     }
 
-    while( List2 != nullptr ){
+    void createList(vector<int>& list) {
+        
+        for (int i = 0; i < list.size(); i++) {
+            head = insertAtFront(head, list[i]);
+        }
+       
+    }
 
-        Node* nexthead;
-        Node* prev ; 
+    void print(Node* head) {
+        Node* current = head;
+        while (current != nullptr) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << "\n";
+    }
+};
 
-            while( List1 != nullptr && List1->data <= List2->data){
-                prev = List1; 
+class Solution {
+public:
+    Node* mergeTwoLists_V1(Node* List1, Node* List2) {
+        if (!List1) return List2;
+        if (!List2) return List1;
+
+        Node* head;
+        if (List1->data <= List2->data) {
+            head = List1;
+        } else {
+            head = List2;
+            List2 = List1;
+            List1 = head;
+        }
+
+        Node* prev = nullptr;
+
+        while (List2 != nullptr) {
+            while (List1 != nullptr && List1->data <= List2->data) {
+                prev = List1;
                 List1 = List1->next;
             }
 
-            nexthead = List2 -> next;
-            List2->next = List1; 
-            prev->next = List2; 
-
-            List1 = List2->next;
-            List2 = nexthead;
-          
-    }
-
-    return head;
-
-}
-
-Node* mergeTwoLists_V2( Node* List1, Node* List2 ){
-    
-    if(!List1) return List2;
-    if(!List2) return List1;
-
-    Node* head;
-    Node* current;
-
-    if( List1->data <= List2->data ){
-        head = List1;
-        List1 = List1->next;
-    }else{
-        head = List2;
-        List2 = List2->next;
-    }
-
-    current = head;
-
-    while( List1 && List2 ){
-        
-        if( List1->data <= List2->data){
-            current->next = List1;
-            List1 = List1->next; 
-        }else{
-            current->next = List2;
-            List2 = List2->next; 
+            Node* nextList2 = List2->next;
+            if (prev != nullptr) {
+                prev->next = List2;
+            }
+            List2->next = List1;
+            prev = List2;
+            List2 = nextList2;
         }
 
-        current = current->next;
+        return head;
     }
 
-    if(!List1){
-        current->next = List2;
-    }
-    if(!List2){
-        current->next = List1;
-    }
+    Node* mergeTwoLists_V2(Node* List1, Node* List2) {
+        if (!List1) return List2;
+        if (!List2) return List1;
 
-    return head;
-}
+        Node* head;
+        Node* current;
 
-Node* mergeTwoListsRecursion( Node* List1, Node* List2 ){
+        if (List1->data <= List2->data) {
+            head = List1;
+            List1 = List1->next;
+        } else {
+            head = List2;
+            List2 = List2->next;
+        }
 
-    if (!List1) return List2;
+        current = head;
 
-    if (!List2) return List1;
+        while (List1 && List2) {
+            if (List1->data <= List2->data) {
+                current->next = List1;
+                List1 = List1->next;
+            } else {
+                current->next = List2;
+                List2 = List2->next;
+            }
+            current = current->next;
+        }
 
-    if( List1->data <= List2->data){
-        List1->next = mergeTwoListsRecursion( List1->next, List2);
-        return List1;
-    }else{
-        List2->next = mergeTwoListsRecursion( List1, List2->next);
-        return List2;
-    }
+        if (!List1) {
+            current->next = List2;
+        }
+        if (!List2) {
+            current->next = List1;
+        }
 
-}
-
-Node* insert( Node* head, int data ){
-    Node* newNode = new Node; 
-    newNode->data = data;
-    newNode->next = head;
-    head = newNode;
-    return head;
-}
-
-void print( Node* head ){
-    Node* current = head;
-
-    while( current != nullptr){
-        cout<<  current->data <<" ";
-        current = current->next;
-    }
-    cout<<"\n";
-}
-
-Node* createList( vector<int>& newlist ){
-
-    Node* list = nullptr;
-
-    for( int i=0; i<newlist.size(); i++ ){
-        list = insert(list, newlist[i]); 
+        return head;
     }
 
-    return list; 
-}
+    Node* mergeTwoListsRecursion(Node* List1, Node* List2) {
+        if (!List1) return List2;
+        if (!List2) return List1;
 
+        if (List1->data <= List2->data) {
+            List1->next = mergeTwoListsRecursion(List1->next, List2);
+            return List1;
+        } else {
+            List2->next = mergeTwoListsRecursion(List1, List2->next);
+            return List2;
+        }
+    }
 
-int main(){
+    void printSolution(Node* head){
+        LinkedList newlist;
+        newlist.print(head);
+    }
 
-    vector< pair< vector<int> , vector<int> > > testCases {
-        { {4,2,1}, {4,3,1}},
-        { { }, { }},
-        { { }, {0}}
+};
+
+void testCases() {
+    vector<pair<vector<int>, vector<int>>> testCases{
+        {{1, 2, 4}, {1, 3, 4}}, //1 1 2 3 4 4
+        {{}, {}}, //
+        {{}, {0}} //0
     };
 
-    cout<<"V1"<<"\n";
+    Solution sol;
 
-    for( auto test : testCases){
-        Node* list1 = createList( test.first ); 
-        Node* list2 = createList( test.second ); 
+    cout<<"Merge V1: \n"; 
 
-        list1 = mergeTwoLists_V1(list1, list2);
-        print(list1);
+    for ( auto test : testCases) {
+        LinkedList list1, list2;
+        list1.createList(test.first);
+        list2.createList(test.second);
 
+        sol.printSolution( sol.mergeTwoLists_V1(list1.head, list2.head));
     }
 
-    cout<<"V2"<<"\n";
+    cout<<"Merge V2: \n"; 
 
-    for( auto test : testCases){
-        Node* list1 = createList( test.first ); 
-        Node* list2 = createList( test.second ); 
+    for ( auto test : testCases) {
+        LinkedList list1, list2;
+        list1.createList(test.first);
+        list2.createList(test.second);
 
-        list1 = mergeTwoLists_V2(list1, list2);
-        print(list1);
-
+        sol.printSolution( sol.mergeTwoLists_V2(list1.head, list2.head));
     }
 
-    cout<<"Recursion"<<"\n";
+    cout<<"Merge Recursion: \n"; 
 
-    for( auto test : testCases){
-        Node* list1 = createList( test.first ); 
-        Node* list2 = createList( test.second ); 
+    for ( auto test : testCases) {
+        LinkedList list1, list2;
+        list1.createList(test.first);
+        list2.createList(test.second);
 
-        list1 = mergeTwoListsRecursion(list1, list2);
-        print(list1);
-
+        sol.printSolution( sol.mergeTwoListsRecursion(list1.head, list2.head));
     }
+    
+}
 
+int main() {
+    testCases();
     return 0;
 }
--- Problems/Recursion/Combinations.cpp
+++ Problems/Recursion/Combinations.cpp
@@ -8,11 +8,30 @@ Given two integers n and k, return all possible combinations of k numbers chosen
 You may return the answer in any order.
 
 Approach: Recursion
-
+The combine function generates all combinations of integers from 1 to range 
+of a specified size sizeSubSet using recursive backtracking. It maintains a 
+subset vector that stores the current combination being constructed. When 
+the size of subset reaches sizeSubSet, it is added to the output vector of 
+vectors that holds all valid combinations. The function iterates through 
+possible elements starting from element up to range, recursively calling 
+itself to explore further combinations by adding each element to subset 
+and then backtracking by removing it before exploring the next possibility. 
+This process continues until all valid combinations of the specified size 
+are generated and stored in output. 
 
 Required Time: 
+
 Complexity Time: 
-Complexity Space: 
+Add this part to the solution its going to cost us lenght K time, because
+there is K elements in subset, because that is the item that 
+we are looking for and calling list on a basically a list of K elements
+is going to take O(k) times so that portion is O(k) and we do this
+for every single combination that we generate so how many combinations
+are there (math formula).
+
+    O(k)*O(nCk) -> O(k)*O(n!/(n-k)k!)
+
+Complexity Space: O(nCk)
 
 Assumpitions: 
 
@@ -22,44 +41,31 @@ Assumpitions:
 #include <vector>
 using namespace std;
 
-void combine(vector<vector<int>>& output, vector<int>&subset, int range, int sizeSubSet, int element ){
+class Solution {
+public:
 
-    if( subset.size( ) == sizeSubSet ){
-        output.push_back( subset ); 
-        return; 
+    void combine(vector<vector<int>>& output, vector<int>&subset, int range, int sizeSubSet, int element ){
+
+        if( subset.size( ) == sizeSubSet ){
+            output.push_back( subset ); 
+            return; 
+        }
+
+        for( int j=element ; j <= range ; j++){
+            subset.push_back(j);
+            combine(output, subset, range, sizeSubSet, j+1 );
+            subset.pop_back(); 
+
+        }
+
     }
 
-    for( int j=element ; j <= range ; j++){
-        subset.push_back(j);
-        combine(output, subset, range, sizeSubSet, j+1 );
-        subset.pop_back(); 
+    void printSolution( int range, int sizeSubSet ){
+        vector<vector<int>> output;
+        vector<int> subset;
+        combine(output, subset, range, sizeSubSet, 1 );
 
-    }
-
-}
-
-vector<vector<int>> combination( int range, int sizeSubSet){
-    vector<vector<int>> output;
-    vector<int> subset;
-    combine(output, subset, range, sizeSubSet, 1 );
-    return output; 
-}
-
-int main(){
-
-    //Test Cases 
-
-    vector<pair<int, int>> testCases {
-        {4, 2},
-        {4, 3},
-        {1,1}
-    };
-
-    for( auto test :  testCases ){
-
-        cout<<"Case: "; 
-        
-        for( auto subset : combination(test.first, test.second ) ){
+        for( auto subset : output ){
             cout<<"[ "; 
             for( auto element : subset ){
                 cout<<element<<" ";
@@ -67,11 +73,31 @@ int main(){
             cout<<"]"; 
             
         }
+        cout<<"\n";
+    }
+};
 
-        cout<<"\n"; 
+void testCases(){
+
+    vector<pair<int, int>> testCases {
+        {4, 2},
+        {4, 3},
+        {1, 1}
+    };
+
+    for( auto test :  testCases ){
+
+        Solution sol;
+        sol.printSolution(test.first, test.second);
+
     }
 
+}
 
+
+int main(){
+    testCases();
+    return 0;
 }
 
 
